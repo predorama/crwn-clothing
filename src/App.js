83d8eb6216eb452b44f878b4c5c019f0ad2ea1d3
Predrag Vila <predorama@gmail.com>
2021-02-03 10:33:32 +0500

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -14,26 +14,24 @@ class App extends React.Component {
     super();
 
     this.state = {
-      currentUser: null
+      currentUser: null,
     };
   }
 
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
+        userRef.onSnapshot((snapShot) => {
           this.setState({
             currentUser: {
               id: snapShot.id,
-              ...snapShot.data()
-            }
+              ...snapShot.data(),
+            },
           });
-
-          console.log(this.state);
         });
       }
 
